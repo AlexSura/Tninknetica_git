131fe1ed20da4bfaf9254c417482f8ad140fdfd5
AlexMed <yellowjazz@mail.ru>
2022-05-30 16:16:23 +0500

    corrected2

--- lesson3/station.rb
+++ lesson3/station.rb
@@ -4,23 +4,37 @@ class Station
   def initialize(name)                     #создание станции (указываем имя)
     @name = name
     @trains = []
+    @trains_pas = []
+    @trains_gruz = [] 
   end
+ 
   def train_reception(train)                       #прием поезда 1н за раз
     trains << train
   end
-  def list_trains_types(type = nil)                            #список по типу поездов
-    if type 
-      puts "На станции #{name} типа #{type}"
-      trains.each {|train| puts train.number if train.type == type}
-    else
-      puts "На станции #{name}"
-      trains.each {|train| puts train.number}
+ 
+  def list_trains_types(type)                            #список по типу поездов
+    if trains.each do |train|
+      if train.type == 'pas'
+        @trains_pas << train.number
+        @trains_pas.uniq!
+      elsif
+        train.type == 'gruz'
+        @trains_gruz << train.number
+        @trains_gruz.uniq!
+      else
+        puts "станция пуста, или неправильно указан тип поезда"
+      end
     end
   end
+    puts "на станции #{name} #{@trains_pas.size} пассажирский поезд(a)"
+    puts "на станции #{name} #{@trains_gruz.size} грузовой поезд(a)"
+  end
+
   def train_dispatch(train)                        #отправка поезда одного за раз указать номер поезда
     @trains.delete(train)
     puts "со станции #{name} отправился поезд #{train.number}"
   end
+ 
   def list_trains
     trains.each{ |train| puts train.number }
   end 
@@ -28,28 +42,32 @@ end
 
 
 class Route
-  attr_reader :stations 
-  #attr_accessor :stations, :first_station, :last_station 
+  attr_reader :stations
 
-  def initialize(first_station, last_station)     #создаем маршрут с указанение начальной и конечной станции
+  def initialize (first_station, last_station)     #создаем маршрут с указанение начальной и конечной станции
+    @first_station = first_station
+    @last_station = last_station
     @stations = [first_station, last_station]
   end
+  
   def add_station(station)                       # метод добавляет промежуточную станцию к маршруту
     self.stations.insert(-2, station)             #insert добвляет елемент в массив по указанному индексу 
     puts "К маршруту #{stations.first.name} - #{stations.last.name} добавлена станция #{station.name}"
   end
+  
   def delete_station(station)                   # удаление промежуточной станции из маршрута
     self.stations.delete(station)
     puts "из маршрута #{stations.first.name} #{stations.last.name} удалена станция: #{station.name}"  
   end
+  
   def list_station                                # выводит список станций маршрута
     stations.each {|station| puts "станция #{station}"}
   end
 end
 
 
+
 class Train 
-  #attr_accessor :speed, :wagons, :number, :station, :route
   attr_reader :type, :number, :route
 
   def initialize(number, type, wagons)            #создание поезда с указанными атрибутами, поезд при создании имеет нулевую скорость
@@ -60,11 +78,12 @@ class Train
     @route = nil
     @current_station = nil
     puts "Поезд #{number} собран и готов к движению, поезд #{type} и имеет #{wagons} вагонов"
-    puts "Прицепка и отцепка вагонов осуществялеться при нулевой скорости поезда и атрибутов 'прицеп','отцеп'"
   end
+ 
   def stop
     @speed = 0
   end
+ 
   def add_wagon
     if self.speed == 0 
       self.wagons+= 1
@@ -72,6 +91,7 @@ class Train
       puts "Остановите поезд что прицепить вагон"
     end
   end
+ 
   def remove_wagon 
     if self.speed == 0 
       self.wagons-= 1
@@ -79,67 +99,61 @@ class Train
       puts "Остановите поезд что отцепить вагон"
     end
   end
+ 
   def take_route(route)                           # поезду присвоен экз класса route т е маршрут
     @route = route
     @current_station = route.stations.first
     route.stations.first.train_reception(self)
     puts "поезду #{number} присвоен маршрут #{route.stations.first.name} #{route.stations.last.name}"
   end                                             #route.stations.first.name доступ к массиву и первому элементу
-  def move_forward_station
-    if @current_station.name!= route.stations.last.name
-     current_i =  route.stations.index(@current_station)
-     route.stations[current_i].train_dispatch(self)
-     current_ii = current_i + 1
-     @current_station = route.stations[current_ii]
-     route.stations[current_ii].train_reception(self)
-     puts " поезд перемещен на станцию #{@current_station.name}"
-   else
-    puts "позед прибыл на конечную станцию #{route.stations.last.name}"
-   end
+
+  def indx
+    indx = route.stations.index(@current_station)
   end
-  def move_back_station
-    if @current_station.name!= route.stations.first.name
-     current_i =  route.stations.index(@current_station)
-     route.stations[current_i].train_dispatch(self)
-     current_ii = current_i - 1
-     @current_station = route.stations[current_ii]
-     route.stations[current_ii].train_reception(self)
-     puts " поезд перемещен на станцию #{@current_station.name}"
-   else
-    puts "поезд находиться на первой станции #{@current_station.name}"
-  end
-  end
+
   def next_station
     if @current_station!= route.stations.last
-      staions_i = route.stations.index(@current_station)
-      next_station = route.stations[staions_i + 1]
-      puts "следующая станция #{route.stations[staions_i + 1].name}"
+      puts "следующая станция #{route.stations[self.indx + 1].name}"
+      return next_station = route.stations[self.indx + 1]
     else
       route.stations.last
       puts "конечная станция #{route.stations.last.name}"
     end
   end
+ 
   def prev_station
     if @current_station!= route.stations.first
-      station_i = route.stations.index(@current_station)
-      prev_station = route.stations[station_i - 1]
-      puts "Предыдущая станция #{prev_station.name}"
+      puts "Предыдущая станция #{route.stations[self.indx - 1].name}"
+      return prev_station = route.stations[self.indx - 1]
     else
       route.stations.first
       puts "Стартовая станция маршрута #{route.stations.first.name}"
     end
   end
-  def adjacent_stations                            # выводит соседнии станции 
-    if route.nil?
-      puts "задайте маршрут"
-    else
-      station_i = route.stations.index(station)
-      puts "Поезд #{number} на станции #{station.name}"
-      puts "Предыдущая станция #{route.stations[station_i - 1].name}" if station_i != 0
-      puts "Следующая станция #{route.stations[station_i + 1].name}" if station_i !=route.stations.size - 1
+
+  def move_forward_station
+    if @current_station.name!= route.stations.last.name
+     route.stations[self.indx].train_dispatch(self)
+     @current_station = self.next_station             
+     @current_station.train_reception(self)
+     puts " поезд перемещен на станцию #{@current_station.name}"
+   else
+    puts "позед прибыл на конечную станцию #{route.stations.last.name}"
+   end
+  end
+ 
+  def move_back_station
+    if @current_station.name!= route.stations.first.name
+     route.stations[self.indx].train_dispatch(self)
+     @current_station = self.prev_station
+     @current_station.train_reception(self)
+     puts " поезд перемещен на станцию #{@current_station.name}"
+   else
+    puts "поезд находиться на первой станции #{@current_station.name}"
     end
   end
 end
+
 
 station0 = Station.new('city1')
 station1 = Station.new('city2')
@@ -161,7 +175,9 @@ station0.train_reception(train1)
 station0.train_reception(train2)
 station0.train_reception(train3)
 
-station0.list_trains_types
+station0.list_trains_types('gruz')
+station0.list_trains_types('pas')
+station0.list_trains
 puts '8' * 80
 station0.list_trains
 station0.train_dispatch(train2)
@@ -178,7 +194,6 @@ marsh.delete_station(station4)
 train4.take_route(marsh)
 
 
-#train4.adjacent_stations
 puts '80'*80
 marsh1 = Route.new(station6, station5)
 marsh1.add_station(station3)
@@ -186,9 +201,8 @@ train4.take_route(marsh1)
 station6.list_trains
 puts '90' * 80
 puts "77"*80
-train4.move_forward_station
-puts '90' * 80
-puts "77"*80
+train4.next_station
+train4.prev_station
 train4.move_forward_station
 train4.move_forward_station
 station5.list_trains
@@ -206,5 +220,5 @@ train4.prev_station
 train4.move_forward_station
 train4.next_station
 train4.prev_station
-
+station5.list_trains_types('pas')
 
