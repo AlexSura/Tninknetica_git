d2255041fc4239a32605d16b602416885f0f68b2
Alex <yellowjazz@mail.ru>
2022-08-20 11:18:49 +0500

    meta2

---
+++ lesson9.1/validate.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Validate
+  NUMBER_TRAIN_VALIDATE = /^[a-z0-9]{3}-*[a-z0-9]{2}$/.freeze
+
+  def validate!(atr)
+    raise 'Number has invalid format!!' if atr.nil?
+  end
+
+  def validate_train!
+    raise 'номер поезда указан неверно!!' if number !~ NUMBER_TRAIN_VALIDATE
+  end
+
+  def valid?
+    validate!
+    true
+  rescue RuntimeError
+    false
+  end
+end
