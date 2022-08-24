33bafa787ee14db5c9435d196bc87e6df32501a8
Alex <yellowjazz@mail.ru>
2022-08-24 17:46:37 +0500

    Delete lesson9.3 directory

--- lesson9.3/manufacturer.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Manufacturer
-  attr_reader :name_manufacturer
-end
--- lesson9.3/passenger_wagons.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class PassengerWagons < Wagon
-  def loading
-    @loading_volume += 1
-  end
-end
